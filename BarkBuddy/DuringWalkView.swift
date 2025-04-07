c5306debd9ae8c54fd736f3ee475372edb6bf1b1
Trinity Storms <stormsthing2@gmail.com>
2025-04-07 16:55:46 -0500

    Added View for mid walk

---
+++ BarkBuddy/DuringWalkView.swift
@@ -0,0 +1,18 @@
+//
+//  DuringWalkView.swift
+//  BarkBuddy
+//
+//  Created by Storms, Trinity on 4/7/25.
+//
+
+import SwiftUI
+
+struct DuringWalkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DuringWalkView()
+}

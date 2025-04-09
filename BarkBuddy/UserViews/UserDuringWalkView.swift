f6158a25150450266766c327310877e14323cf87
Trinity Storms <stormsthing2@gmail.com>
2025-04-09 16:52:48 -0500

    Added UI view for pet owners mid walk.

---
+++ BarkBuddy/UserViews/UserDuringWalkView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDuringWalkView.swift
+//  BarkBuddy
+//
+//  Created by Storms, Trinity on 4/9/25.
+//
+
+import SwiftUI
+
+struct UserDuringWalkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDuringWalkView()
+}
